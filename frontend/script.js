72f2d85b99e4783f5e7d4a0063ac362f98b4d00e
Visnu15 <psvisnu15@gmail.com>
2026-03-16 21:16:34 +0530

    Added structify landing page

---
+++ frontend/script.js
@@ -0,0 +1,9 @@
+const builders = [
+  {name:"ABC Builders", location:"Chennai", price:2000},
+  {name:"Urban Homes", location:"Chennai", price:2500},
+  {name:"Dream Constructions", location:"Bangalore", price:1800}
+];
+
+function searchBuilders(){
+
+}
